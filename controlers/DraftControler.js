356ee7c652c4cfc9b387df2863286d5acec958db
Sl0wdive <vladggobona@gmail.com>
2023-03-19 00:58:54 +0200

    drafts prototype fixed

--- controlers/DraftControler.js
+++ controlers/DraftControler.js
@@ -2,31 +2,31 @@ import DraftModel from "../models/Draft.js";
 
 export const create = async (req, res) => {
     try {
-      const doc = new DraftModel({
-        sender: req.userId,
-        content: req.body.content,
-      });
-  
-      const post = await doc.save();
-  
-      res.json(post);
+        const doc = new DraftModel({
+            sender: req.userId,
+            content: req.body.content,
+        });
+        
+        const post = await doc.save();
+        
+        res.json(post);
     } catch (err) {
-      console.log(err);
-      res.status(500).json({
-        message: 'Failed to create new draft',
-      });
+        console.log(err);
+        res.status(500).json({
+            message: 'Failed to create new draft',
+        });
     }
-  };
+};
 
-  export const getOne = async (req, res) => {
+export const getOne = async (req, res) => {
     try {
         const drafts = await DraftModel.find();
         drafts.reverse();
         res.json(drafts);
-      } catch (err) {
+    } catch (err) {
         console.log(err);
         res.status(500).json({
-          message: 'Failed to receive post',
+            message: 'Failed to receive post',
         });
-      }
-    };
+    }
+};
